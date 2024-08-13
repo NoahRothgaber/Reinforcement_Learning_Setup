93c51aec71c964f092c708cd44b0aa0356a1ba7f
Noah Rothgaber <noahrothgaber@gmail.com>
2024-08-13 15:06:27 -0400

    Fixing user issue take 3! :(

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-username=$USER
+username=$(logname)
 apt -y upgrade
 apt -y install python3.10
 snap install code --classic
 apt -y install python3-pip
-sudo -u username pip install torch
+sudo -u $username pip install torch
 apt -y install swig
 apt -y install git
-sudo -u username pip installls
- matplotlib
-sudo -u username pip install gymnasium
-sudo -u username pip install gymnasium[box2d]
-sudo -u username pip install gymnasium[classic-control]
+sudo -u $username pip install matplotlib
+sudo -u $username pip install gymnasium
+sudo -u $username pip install gymnasium[box2d]
+sudo -u $username pip install gymnasium[classic-control]
 git clone -b categorical https://github.com/NoahRothgaber/reinforcement_learning
 cd reinforcement_learning/rainbow_dqn_pytorch
 python3 agent.py cartpole1 --train
