99ced0d1665c58e82816f97ff009476642ca251f
Noah Rothgaber <noahrothgaber@gmail.com>
2024-08-13 14:47:37 -0400

    Fixed install of box2d, added lines for testing, will remove lines 13-15 in the future.ls

--- setup.sh
+++ setup.sh
@@ -6,7 +6,10 @@ apt -y install python3-pip
 pip install torch
 apt -y install swig
 apt -y install git
-pip install matplotlibpip
+pip install matplotlib
 pip install gymnasium
 pip install gymnasium[box2d]
 pip install gymnasium[classic-control]
+git clone -b categorical https://github.com/NoahRothgaber/reinforcement_learning
+cd reinforcement_learning/rainbow_dqn
+python3 agent.py cartpole1 --train
