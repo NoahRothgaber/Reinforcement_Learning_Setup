b5aa4bc7ffd95af3944b8b714d97beb03b42c09f
Noah Rothgaber <noahrothgaber@gmail.com>
2024-08-13 14:23:52 -0400

    Added dependencies for getting gym setup, will be adding ros2 next.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-
+apt -y upgrade
+apt -y install python3.10
+snap install code --classic
+apt -y install python3-pip
+pip install torch
+apt -y install swig
+pip install box2d
+apt -y install git
+pip install matplotlib
