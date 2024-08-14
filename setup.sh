e9d28665c6efddcb3ee35449d3209d1b09f5d3e8
Noah Rothgaber <noahrothgaber@gmail.com>
2024-08-14 11:32:44 -0400

    Addressing issues with directory navigation.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ username=$(logname)
 apt -y upgrade
 apt -y install python3.10
 snap install code --classic
+
 apt -y install python3-pip
 sudo -u $username pip install torch
 apt -y install swig
@@ -12,5 +13,5 @@ sudo -u $username pip install gymnasium
 sudo -u $username pip install gymnasium[box2d]
 sudo -u $username pip install gymnasium[classic-control]
 sudo -u $username git clone -b categorical https://github.com/NoahRothgaber/reinforcement_learning
-sudo -u $username cd reinforcement_learning/rainbow_dqn_pytorch
+cd /home/$username/reinforcement_learning/rainbow_dqn_pytorch
 sudo -u $username python3 agent.py cartpole1 --train
