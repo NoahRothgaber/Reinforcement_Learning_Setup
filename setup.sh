6c776c845b2508b8b21a207c4e8a75b234ab8406
Noah Rothgaber <noahrothgaber@gmail.com>
2024-08-14 11:15:24 -0400

    Editing git clone command to (hopefully) resolve issues with creating directory permissions.

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ sudo -u $username pip install matplotlib
 sudo -u $username pip install gymnasium
 sudo -u $username pip install gymnasium[box2d]
 sudo -u $username pip install gymnasium[classic-control]
-git clone -b categorical https://github.com/NoahRothgaber/reinforcement_learning
+sudo -u git clone -b categorical https://github.com/NoahRothgaber/reinforcement_learning
 cd reinforcement_learning/rainbow_dqn_pytorch
 python3 agent.py cartpole1 --train
