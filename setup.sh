8c06ff0c3455c8b44f00759795ac09182ddd7e5a
Noah Rothgaber <noahrothgaber@gmail.com>
2024-08-14 11:42:34 -0400

    Adjusted structure, file will be cloned into home directory, no need for folder.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ username=$(logname)
 apt -y upgrade
 apt -y install python3.10
 snap install code --classic
-
 apt -y install python3-pip
 sudo -u $username pip install torch
 apt -y install swig
@@ -12,6 +11,6 @@ sudo -u $username pip install matplotlib
 sudo -u $username pip install gymnasium
 sudo -u $username pip install gymnasium[box2d]
 sudo -u $username pip install gymnasium[classic-control]
+cd /home/$username
 sudo -u $username git clone -b categorical https://github.com/NoahRothgaber/reinforcement_learning
-cd /home/$username/reinforcement_learning_setup/reinforcement_learning/rainbow_dqn_pytorch
 sudo -u $username python3 agent.py cartpole1 --train
